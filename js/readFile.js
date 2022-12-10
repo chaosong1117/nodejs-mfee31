1432f05aa199f4c8a0f24778910ae00c3e7ca31e
chaosong1117 <a0922568671@gmail.com>
2022-12-10 12:27:46 +0800

    [feat]修改readFile.js

--- js/readFile.js
+++ js/readFile.js
@@ -2,14 +2,14 @@ const fs = require('fs')
 const { resolve } = require('path')
 
 let file = new Promise((resolve, reject) => {
-  fs.readFile('test.txt', 'utf-8', (err, data) => {
+  fs.readFile('test1.txt', 'utf-8', (err, data) => {
     if(err) reject(err)
     resolve(data)
   })
 })
 
 file.then((data) => {
-  console.log('這裡是 then', data, file)
+  console.log('讀檔內容為', data, file)
 }).catch((error) => {
-  console.error('這裡是error', error, file)
+  console.error('錯誤訊息是', error, file)
 })
