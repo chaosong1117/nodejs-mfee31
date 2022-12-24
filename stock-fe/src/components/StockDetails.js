dd3580208acce1e37f168b8318fa02c1b627dc88
chaosong1117 <a0922568671@gmail.com>
2022-12-24 16:09:24 +0800

    [feat]修改StockDetails.js

--- stock-fe/src/components/StockDetails.js
+++ stock-fe/src/components/StockDetails.js
@@ -11,7 +11,9 @@ const StockDetails = () => {
     // 在 component 初始化的時候跑一次
     // 通常會把去跟後端要資料的動作放在這裡
     async function getStocks() {
-      let response = await axios.get(`http://localhost:3001/api/stocks/${stockId}`);
+      let response = await axios.get(
+        `http://localhost:3001/api/stocks/${stockId}`
+      );
       setStocks(response.data);
     }
     getStocks();
@@ -39,40 +41,39 @@ const StockDetails = () => {
         </li>
       </ul>
       目前在第 1 頁
-      {stocks.map((stock) => {
+      {stocks.map((stock, index) => {
         return (
-          <div className="bg-white bg-gray-50 p-6 rounded-lg shadow m-6">
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">
-          日期: {stock.date}
-        </h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">
-          成交金額: {stock.amount}
-        </h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">
-          成交股數: {stock.volumn}
-        </h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">
-          開盤價：{stock.open_price}
-        </h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">
-          收盤價：{stock.close_price}
-        </h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">
-          漲跌價差：{stock.delta_price}
-        </h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">
-          最高價：{stock.high_price}
-        </h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">
-          最低價：{stock.low_price}
-        </h2>
-        <h2 className="text-2xl font-bold mb-2 text-gray-800">
-          成交筆數：{stock.transactions}
-        </h2>
-      </div>
-        )
+          <div className="bg-white bg-gray-50 p-6 rounded-lg shadow m-6" key={index}>
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">
+              日期: {stock.date}
+            </h2>
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">
+              成交金額: {stock.amount}
+            </h2>
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">
+              成交股數: {stock.volumn}
+            </h2>
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">
+              開盤價：{stock.open_price}
+            </h2>
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">
+              收盤價：{stock.close_price}
+            </h2>
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">
+              漲跌價差：{stock.delta_price}
+            </h2>
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">
+              最高價：{stock.high_price}
+            </h2>
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">
+              最低價：{stock.low_price}
+            </h2>
+            <h2 className="text-2xl font-bold mb-2 text-gray-800">
+              成交筆數：{stock.transactions}
+            </h2>
+          </div>
+        );
       })}
-      
     </div>
   );
 };
